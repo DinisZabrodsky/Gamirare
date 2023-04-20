892a1d4488578e3ed98753ab9c2071a167e5c62c
Dinis PC <DinisZabrodsky@gmail.com>
2023-04-20 18:48:47 +0300

    js code

--- src/about.js
+++ src/about.js
@@ -3,3 +3,5 @@ import comeback from "./js/come-back";
 import socialIcon from "./js/social-icon";
 import feedback from "./js/feedback";
 import feedbackLocalStr from "./js/feedbackLocalStr";
+
+import whatsNewCreateContent from "./js/whats-new-crate-content";
---
+++ src/js/whats-new-crate-content.js
@@ -0,0 +1,28 @@
+import { dataForWhatsNew } from "../data/whats-new-data";
+
+const whatsNewSection = document.querySelector('#whats-new-list');
+
+addCardOnPage(dataForWhatsNew);
+
+function addCardOnPage(data) {
+    const htmlCode = createCard(data);
+    whatsNewSection.innerHTML = htmlCode;
+    console.log(htmlCode);
+}
+
+
+function createCard (elements) {
+    return elements.map(({img, alt, date, titel, text, id}) => {
+        return `
+        <li class="whats-new-list-element">
+        <div class="whats-new-list-element-img">
+            <img src=${img} alt="${alt}">
+        </div>
+        <div class="whats-new-list-element-content">
+            <p class="wn-date">${date}</p>
+            <h3 class="wn-titel">${titel}</h3>
+            <p class="wn-text">${text}</p>
+            <button class="wn-button" type="button"  data-id="${id}">Читати далі</button>
+        </div>
+        </li>`}).join('');
+}
